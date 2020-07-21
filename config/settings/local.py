b1d097fd7e6f27e10bcc835c8441ab411fa81676
Victor Ravelo <vravelolv4@gmail.com>
2020-07-21 09:43:49 -0400

    first stage

---
+++ config/settings/local.py
@@ -0,0 +1,9 @@
+from .base import *
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'x6^$q-!8knxez84yld@1!ay+-8&)gp9-8ncyf+zzv%qr-8pp=4'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
